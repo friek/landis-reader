d3f36941e2bc8b1eee597181f77bf4fb4664b208
Johan Mulder <johan@mulder.net>
2025-07-24 20:25:25 +0200

    Checking for data timeout

--- src/main.py
+++ src/main.py
@@ -9,23 +9,29 @@ logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
+class SocketClosedException(Exception):
+    pass
+
+
 # noinspection PyUnusedLocal
 def on_disconnect(client, userdata, rc):
     if rc != mqtt.MQTT_ERR_SUCCESS:
-        raise Exception(f"MQTT socket closed with exit code {rc}")
+        raise SocketClosedException(f"MQTT socket closed with exit code {rc}")
 
 
 if __name__ == '__main__':
-    c = mqtt.Client()
-    c.connect(host=os.getenv("MQTT_HOST"), port=int(os.getenv("MQTT_PORT", 1883)))
-    c.on_disconnect = on_disconnect
-    c.loop_start()
+    client = mqtt.Client()
+    client.connect(host=os.getenv("MQTT_HOST"), port=int(os.getenv("MQTT_PORT", 1883)))
+    client.on_disconnect = on_disconnect
+    client.loop_start()
 
     p1_reader = Reader(port=os.getenv("P1_PORT"))
     topic = os.getenv("ENERGY_TOPIC")
     raw_topic = os.getenv("ENERGY_RAW_TOPIC")
 
     for message, raw_message in p1_reader.read_forever():
-        c.publish(topic=topic, payload=message.to_json())
-        c.publish(topic=raw_topic, payload=raw_message)
+        if topic:
+            client.publish(topic=topic, payload=message.to_json())
+        if raw_topic:
+            client.publish(topic=raw_topic, payload=raw_message)
         LOGGER.info("Broadcasted message")
--- src/p1/reader.py
+++ src/p1/reader.py
@@ -1,13 +1,19 @@
 from typing import Generator
 
 import serial
+from datetime import datetime
 
 from p1.converter import MeterReading, Converter
 
 
+class NoDataException(Exception):
+    pass
+
+
 class Reader:
     def __init__(self, baudrate=115200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN,
-                 stopbits=serial.STOPBITS_ONE, xonxoff=0, rtscts=0, timeout=20, port="/dev/ttyUSB0"):
+                 stopbits=serial.STOPBITS_ONE, xonxoff=0, rtscts=0, timeout=20, port="/dev/ttyUSB0",
+                 data_timeout: int=60):
         ser = serial.Serial()
         ser.baudrate = baudrate
         ser.bytesize = bytesize
@@ -21,23 +27,34 @@ class Reader:
         ser.open()
         self._ser = ser
         self._converter = Converter()
+        self.data_timeout = data_timeout
 
     def read_forever(self) -> Generator[MeterReading, None, None]:
-        message = ""
+        telegram = bytearray()
+        last_read = None
 
         while True:
             try:
                 p1_raw = self._ser.readline()
+                now = datetime.now()
+                telegram.extend(p1_raw)
+
+                if len(p1_raw) > 5:
+                    last_read = now
+                else:
+                    if last_read and (now - last_read).total_seconds() > self.data_timeout:
+                        raise NoDataException(f"No data received for {self.data_timeout} seconds")
+
                 line = str(p1_raw, encoding="utf-8").strip()
-                message += line + "\n"
-                print(line)
                 if len(line) < 1:
                     continue
 
                 if line[0] == '!':
                     # End of message
+                    message = telegram.decode(encoding='utf-8', errors='replace')
+                    print(message)
                     yield self._converter.convert_p1_message(message), message
-                    message = ""
+                    telegram.clear()
             except:
                 self._ser.close()
                 raise
